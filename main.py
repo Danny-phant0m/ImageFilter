9f81de9bfef7bbecf428f418e17f11ce9384851d
unknown <dixondaniel434@gmail.com>
2024-03-24 00:06:42 +0200

    Aplly filters

--- main.py
+++ main.py
@@ -20,6 +20,20 @@ if __name__ == "__main__":
         print("4. Color Swapping")
         filter_number = int(input())
 
+        # Apply the filter based on choice
+        if filter_number == 1:
+            filtered_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        elif filter_number == 2:
+            filtered_image = cv2.GaussianBlur(img, (5, 5), 0)
+        elif filter_number == 3:
+            filtered_image = cv2.Canny(img, 100, 200) 
+        elif filter_number == 4:
+            B, G, R = cv2.split(img)
+            filtered_image = cv2.merge((R, B, G))
+        else:
+            print("Invalid filter choice. Please choose from 1-4.")
+            exit()
+
     except ValueError:
         print("That's not an int!")
 
