d38cf68f5e6c16db94be7f4e7a50dcad78909806
unknown <dixondaniel434@gmail.com>
2024-03-24 00:04:54 +0200

    Ask user for filter to apply

--- main.py
+++ main.py
@@ -13,7 +13,13 @@ if __name__ == "__main__":
         # Save image in set directory
         img = cv2.imread('images/Image_'+str(image_number)+'.jpg')# loads the image to open cv
 
-        
+        print("Choose a filter to apply to the image")
+        print("1. Grayscale")
+        print("2. Blurring (Gaussian)")
+        print("3. Edge Detection (Canny)")
+        print("4. Color Swapping")
+        filter_number = int(input())
+
     except ValueError:
         print("That's not an int!")
 
